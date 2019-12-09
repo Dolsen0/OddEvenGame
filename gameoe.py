bbd57358d4458d7c548891e21f00dffa5ca2ebef
Dolsen0 <53360736+Dolsen0@users.noreply.github.com>
2019-12-09 12:56:31 -0500

    Add files via upload

---
+++ gameoe.py
@@ -0,0 +1,18 @@
+from random import randint
+
+g = [1, 2]
+
+comp_move = g[randint(0, 1)]
+player = False
+
+
+while player == False:
+    player = input("Do you choose 1 or 2?")
+    move = int(player) + int(comp_move)
+    print("I choose", comp_move)
+    if move % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+    print(move)
+    player = False
