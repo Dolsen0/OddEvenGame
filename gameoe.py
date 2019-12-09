cb7f237981d76437c8cec41c3a1461e4034f8ba7
Dolsen0 <53360736+Dolsen0@users.noreply.github.com>
2019-12-09 14:49:32 -0500

    Update gameoe.py

--- gameoe.py
+++ gameoe.py
@@ -2,21 +2,27 @@ from random import randint
 
 g = [1, 2]
 
-comp_move = g[randint(0, 1)]
 player = False
 
 
 while player == False:
+    comp_move = g[randint(0, 1)]
+    choice = input("Odd or Even")
     player = input("Do you choose 1 or 2?")
-    
     move = int(player) + int(comp_move)
-    
+
     print("I choose", comp_move)
-    
-    
-    if move % 2 == 0:
-        print("Even")
-    else:
-        print("Odd")
-    print(move)
+
+    if choice == "Even":
+        if move % 2 == 0:
+            print("you win, the result is even")
+        else:
+            print("you lose, the result is odd")
+    elif choice == "Odd":
+        if move % 2 == 1:
+            print("You Win! The result is odd")
+        else:
+            print("You lose, the result is even")
+    print("the total is", move)
+
     player = False
